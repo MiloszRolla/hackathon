d60ac8e002c398d97b0726c2375f8d5217ee67a5
Milosz <miloszrolla@gmail.com>
2022-12-02 21:46:13 +0100

    mui install, login page background

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+export default function login() {
+  return (
+    <>
+    <Box sx={{display:'flex', justifyContent:'end', background:'#FAF3DD', width:'100vw', height:'100vh'}}>
+        <Box sx={{width:'30vw', height:'100vh', background:'#94D8E1'}}>
+          
+        </Box>
+    </Box>
+    </>
+  )
+}
