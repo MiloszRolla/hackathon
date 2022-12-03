5694badc18b5d8d03497b6e36d80dc45e1e8e5d6
Veranex <Maciek.henkel@gmail.com>
2022-12-03 12:43:15 +0100

    emotion charts api test

--- pages/data.js
+++ pages/data.js
@@ -1,30 +1,14 @@
-export const UserData = [
-    {
-        id: 1,
-        mood: 3,
-    },
-    {
-        id: 2,
-        mood: 1,
-    },
-    {
-        id: 3,
-        mood: 2,
-    },
-    {
-        id: 4,
-        mood: 3,
-    },
-    {
-        id: 5,
-        mood: 3,
-    },
-    {
-        id: 6,
-        mood: 5,
-    },
-    {
-        id: 7,
-        mood: 4,
-    },
-]
+import {useEffect, useState} from "react";
+import React from "react";
+import axios from "axios";
+async function loadText() {
+    const response = await fetch('https://firebasestorage.googleapis.com/v0/b/hackathon-d6832.appspot.com/o/emotions.json?alt=media&token=c988c5ef-4425-43d5-abd5-defa3d9c38c5');
+    const UserData=await response.json();
+   console.log(UserData[0].mood);
+}
+
+export default function Data() {
+
+
+ loadText();
+}
--- pages/test.js
+++ pages/test.js
@@ -1,33 +1,51 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BarChart from "../components/BarChart";
-import {UserData} from './data'
+import axios from "axios";
+
+function test(){
+
+  const [testData, setTestData] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get('https://firebasestorage.googleapis.com/v0/b/hackathon-d6832.appspot.com/o/emotions.json?alt=media&token=c988c5ef-4425-43d5-abd5-defa3d9c38c5');
+    setTestData(res.data)
+    console.log(res.data)
+  }
+
+  useEffect(() => {
+    fetchData();
+
+  }, []);
+
+
+
 function counter(UserData){
   const a=[]
   
-  a[0]=UserData.filter((data) => data.mood==1).length
-  a[1]=UserData.filter((data) => data.mood==2).length
-  a[2]=UserData.filter((data) => data.mood==3).length
-  a[3]=UserData.filter((data) => data.mood==4).length
-  a[4]=UserData.filter((data) => data.mood==5).length
+  a[0]=loadText.UserData.filter((data) => data.mood==1).length
+  a[1]=loadText.UserData.filter((data) => data.mood==2).length
+  a[2]=loadText.UserData.filter((data) => data.mood==3).length
+  a[3]=loadText.UserData.filter((data) => data.mood==4).length
+  a[4]=loadText.UserData.filter((data) => data.mood==5).length
 return a
 
 }
-function test(){
+
   const [userData, setUserData] = useState({
     labels: [1,2,3,4,5],
     datasets: [
-      {
-        label: "Mood Lvl",
-        data: counter(UserData),
-        backgroundColor: [
-          "#FFA69E",
-          "#D17F7F",
-          "#FF8282",
-          "#FFBFBF",
-          "#FFE8E8"
-        ]
-      },
+      // {
+      //   label: "Mood Lvl",
+      //   data: counter(loadText.UserData),
+      //   backgroundColor: [
+      //     "#FFA69E",
+      //     "#D17F7F",
+      //     "#FF8282",
+      //     "#FFBFBF",
+      //     "#FFE8E8"
+      //   ]
+      // },
     ],
   })
 
@@ -36,6 +54,7 @@ function test(){
     <div style={{width: 400 }}>
     <BarChart chartData={userData}/>
   </div>
+  <div>{}</div>
   </center>
   </div>
 }
