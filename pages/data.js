a806ae102c1fe7a1962009e76a4f3f343590c223
Veranex <Maciek.henkel@gmail.com>
2022-12-03 07:22:08 +0100

    Charts added

---
+++ pages/data.js
@@ -0,0 +1,30 @@
+export const UserData = [
+    {
+        id: 1,
+        mood: 3,
+    },
+    {
+        id: 2,
+        mood: 1,
+    },
+    {
+        id: 3,
+        mood: 2,
+    },
+    {
+        id: 4,
+        mood: 3,
+    },
+    {
+        id: 5,
+        mood: 3,
+    },
+    {
+        id: 6,
+        mood: 5,
+    },
+    {
+        id: 7,
+        mood: 4,
+    },
+]
